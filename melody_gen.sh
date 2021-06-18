3e4bdea8db0edb95bdafa37e8b6d96bcbd9a60d8
Shivali Kattumadam <shivali.kattumadam@gmail.com>
2021-06-17 17:03:36 -0700

    change directory name to bundles

--- melody_gen.sh
+++ melody_gen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-melody_rnn_generate --config=attention_rnn --bundle_file=attention_rnn.mag --output_dir=./output/generated --num_output=10 --num_steps=128 --primer_melody="[70]"
+melody_rnn_generate --config=attention_rnn --bundle_file=bundles/attention_rnn.mag --output_dir=./output/generated --num_output=10 --num_steps=128 --primer_melody="[60]"
